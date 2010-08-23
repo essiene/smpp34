bd7319a3e2d802ed452803baa7afb3747eaf0b9c
Essien Ita Essien <essiene@gmail.com>
2010-08-23 15:47:32 +0100

    Hookup smpp34_snum_sup under the toplevel smpp34_sup
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_sup.erl
+++ src/smpp34_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, infinity, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,5 +24,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, { {one_for_one, 5, 10}, [?CHILD(smpp34_snum_sup, supervisor)]} }.
 
