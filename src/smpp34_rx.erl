e5659c6441c3cba4803bc3790b9cd9d272441f68
Essien Ita Essien <essiene@gmail.com>
2010-08-23 19:49:59 +0100

    Add first implementation of smpp34_rx module
    This module handles the RX channel of the communication.
    
    After a connection is setup, the socket is passed into
    this module, which then does a continous receive on the
    socket (blocking if it needs to).
    
    It then calls smpp34pdu:unpack/1 on received binaries and
    delivers the unpacked PDUs via a message to the PduSink
    process.
    
    It also uses erlang:monitor/2 to watch the OWNER process and
    will exit when the OWNER goes down.
    
    If the socket closes or there is a tcp_error, it also dies
    and propagates that death upwards to any process that is
    monitoring it... (we're looking at you... OWNER!!!) ;)
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34_rx.erl
@@ -0,0 +1,70 @@
+-module(smpp34_rx).
+-include_lib("smpp34pdu/include/smpp34pdu.hrl").
+-include("util.hrl").
+-behaviour(gen_server).
+
+-export([start_link/3, stop/1]).
+
+-export([init/1,
+        handle_call/3,
+        handle_cast/2,
+        handle_info/2,
+        terminate/2,
+        code_change/3]).
+
+
+-record(state, {owner,monitref,socket,pdu_sink,data}).
+
+start_link(Owner, Socket, PduSink) ->
+    gen_server:start_link(?MODULE, [Owner, Socket, PduSink], []).
+
+stop(Pid) ->
+    gen_server:cast(Pid, stop).
+
+init([Owner, Socket, PduSink]) ->
+	{ok, MonitorRef} = erlang:monitor(process, Owner),
+    inet:setopts(Socket, [{active, once}]),
+    {ok, #state{owner=Owner, monitref=MonitorRef, socket=Socket,
+				   pdu_sink=PduSink, data = <<>>}}.
+
+handle_call(Req, _From, St) ->
+    {reply, {error, Req}, St}.
+
+handle_cast(stop, St) ->
+    {stop, normal, St};
+handle_cast(_Req, St) ->
+    {noreply, St}.
+
+handle_info({tcp, Socket, Data}, #state{socket=Socket, data=Data0, pdu_sink=PduSink}=St) ->
+    Data1 = <<Data0/binary,Data/binary>>,
+	{_, PduList, Rest} = smpp34pdu:unpack(Data1), 
+	notify(PduSink, PduList), 
+	inet:setopts(Socket, [{active, once}]), 
+	{noreply, St#state{data=Rest}};
+handle_info({tcp_closed, Socket}, #state{socket=Socket}=St) ->
+	{stop, tcp_closed, St};
+handle_info({tcp_error, Socket, Reason}, #state{socket=Socket}=St) ->
+	% Well, I don't think it makes sense to attempt to 
+	% continue when a TCP error occurs. Better bail here, so
+	% the monitoring process will also bail.
+
+	{stop, {tcp_error, Reason}, St};
+handle_info(#'DOWN'{ref=MonitorRef}, #state{monitref=MonitorRef}=St) ->
+	{stop, normal, St};
+handle_info(_Req, St) ->
+	{noreply, St}.
+
+terminate(_Reason, _St) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {noreply, St}.
+
+
+notify(_, []) ->
+    ok;
+notify(Pid, [Head|Rest]) ->
+    notify(Pid, Head),
+    notify(Pid, Rest);
+notify(Pid, Event) ->
+    Pid ! {self(), Event}.
