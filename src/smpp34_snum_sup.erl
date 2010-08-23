d11c5d9b4101541b6fe3333f9841266b38018279
Essien Ita Essien <essiene@gmail.com>
2010-08-23 14:57:23 +0100

    Add simple_one_for_one supervisor to manage smpp34_snum
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34_snum_sup.erl
@@ -0,0 +1,31 @@
+
+-module(smpp34_snum_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start_child/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, transient, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_child() ->
+	supervisor:start_child(?MODULE, [self()]).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(smpp34_snum, worker)]} }.
+
