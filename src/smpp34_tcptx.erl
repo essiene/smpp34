ad9ab124ea601a506bd6fa8281680a735bf29e8b
Essien Ita Essien <essiene@gmail.com>
2010-08-24 21:09:25 +0100

    Add tcptx supervisor and tcptx module proper
    The tcptx module is responsible for any sending
    of PDU out to the network. It takes in a PDU
    via send/3 or send/4 and calls smpp34pdu:pack/3 to
    convert it to a BIN then send it off the network.
    
    It also starts and keeps a private copy of its own
    smpp34_snum instance.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34_tcptx.erl
@@ -0,0 +1,74 @@
+-module(smpp34_tcptx).
+-include_lib("smpp34pdu/include/smpp34pdu.hrl").
+-include("util.hrl").
+-behaviour(gen_server).
+
+-export([start_link/2, stop/1]).
+-export([send/3, send/4, ping/1]).
+
+-export([init/1,
+        handle_call/3,
+        handle_cast/2,
+        handle_info/2,
+        terminate/2,
+        code_change/3]).
+
+
+-record(state, {owner,monitref,socket,snum, snum_monitref}).
+
+start_link(Owner, Socket) ->
+    gen_server:start_link(?MODULE, [Owner, Socket], []).
+
+stop(Pid) ->
+    gen_server:cast(Pid, stop).
+
+send(Pid, Status, Body) ->
+	gen_server:call(Pid, {send, Status, Body}).
+
+send(Pid, Status, Snum, Body) ->
+	gen_server:call(Pid, {send, Status, Snum, Body}).
+
+ping(Pid) ->
+	gen_server:call(Pid, ping).
+
+
+
+init([Owner, Socket]) ->
+	MonitorRef = erlang:monitor(process, Owner),
+	{ok, Snum} = smpp34_snum_sup:start_child(),
+	SnumMonitRef = erlang:monitor(process, Snum),
+    {ok, #state{owner=Owner, monitref=MonitorRef, socket=Socket,
+				   snum=Snum, snum_monitref=SnumMonitRef}}.
+
+handle_call(ping, _From, #state{owner=Owner, socket=Socket, snum=Snum}=St) ->
+	{reply, {pong, [{owner, Owner}, {socket, Socket}, {snum, Snum}]}, St};
+handle_call({send, Status, Body}, _From, 
+				#state{socket=Socket, snum=Snum}=St) ->
+	{ok, Num} = smpp34_snum:next(Snum),
+	Bin = smpp34pdu:pack(Status, Num, Body),
+	ok = gen_tcp:send(Socket, Bin),
+	{reply, {ok, Num}, St};
+handle_call({send, Status, Num, Body},_From, #state{socket=Socket}=St)->
+	Bin = smpp34pdu:pack(Status, Num, Body),
+	ok = gen_tcp:send(Socket, Bin),
+	{reply, {ok, Num}, St};
+handle_call(Req, _From, St) ->
+    {reply, {error, Req}, St}.
+
+handle_cast(stop, St) ->
+    {stop, normal, St};
+handle_cast(_Req, St) ->
+    {noreply, St}.
+
+handle_info(#'DOWN'{ref=MonitorRef}, #state{monitref=MonitorRef}=St) ->
+	{stop, normal, St};
+handle_info(#'DOWN'{ref=SnumMonitRef}, #state{snum_monitref=SnumMonitRef}=St) ->
+	{stop, normal, St};
+handle_info(_Req, St) ->
+	{noreply, St}.
+
+terminate(_Reason, _St) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {noreply, St}.
