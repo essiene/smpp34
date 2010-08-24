1468e35c6e36da2d7d5b4d9da2ba05a5280f9579
Essien Ita Essien <essiene@gmail.com>
2010-08-24 21:12:54 +0100

    Implement pdurx supervisor and module proper
    PDURX is the component which receives PDUs from
    TCPRX and either responds to it immediately (enquire_link,
    unbind) or delivers it to another process.
    
    When responding immediately, it will send its response PDUs
    to TCPTX for sending.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34_pdurx.erl
@@ -0,0 +1,61 @@
+-module(smpp34_pdurx).
+-include_lib("smpp34pdu/include/smpp34pdu.hrl").
+-include("util.hrl").
+-behaviour(gen_server).
+
+-export([start_link/2,stop/1]).
+-export([ping/1]).
+
+-export([init/1,
+        handle_call/3,
+        handle_cast/2,
+        handle_info/2,
+        terminate/2,
+        code_change/3]).
+
+-record(state, {owner, monitref, tx}).
+
+start_link(Owner, Tx) ->
+    gen_server:start_link(?MODULE, [Owner, Tx], []).
+
+stop(Pid) ->
+    gen_server:cast(Pid, stop).
+
+ping(Pid) ->
+	gen_server:call(Pid, ping).
+
+init([Owner, Tx]) ->
+	MonitorRef = erlang:monitor(process, Owner),
+    {ok, #state{owner=Owner, monitref=MonitorRef, tx=Tx}}.
+
+handle_call(ping, _From, #state{owner=Owner, tx=Tx}=St) ->
+	{reply, {pong, [{owner,Owner}, {tx,Tx}]}, St};
+handle_call(Req, _From, St) ->
+    {reply, {error, Req}, St}.
+
+handle_cast(stop, St) ->
+    {stop, normal, St};
+handle_cast(_Req, St) ->
+    {noreply, St}.
+
+handle_info(#pdu{sequence_number=Snum, body=#enquire_link{}},
+		    #state{tx=Tx}=St) ->	
+	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #enquire_link_resp{}),
+	{noreply, St};
+handle_info(#pdu{sequence_number=Snum, body=#unbind{}},
+		    #state{tx=Tx}=St) ->	
+	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #unbind_resp{}),
+	{stop, unbind, St};
+handle_info(#pdu{}=Pdu, #state{owner=Owner}=St) ->
+	Owner ! {self(), Pdu},
+	{noreply, St};
+handle_info(#'DOWN'{ref=MonitorRef}, #state{monitref=MonitorRef}=St) ->
+	{stop, normal, St};
+handle_info(_Req, St) ->
+    {noreply, St}.
+
+terminate(normal, _St) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {noreply, St}.
