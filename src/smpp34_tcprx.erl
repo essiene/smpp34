b5cdd689a5a8fc770d270f6613d9668cf3708a64
Essien Ita Essien <essiene@gmail.com>
2010-08-26 20:35:53 +0100

    Update smpp34_tcprx to work with pdurx name change
    Also add a few more cosmetic changes:
    
    	* Rename pdurx to pdusink, it's less confusing than
    	  a generic 'rx'
    	* Rename monitref to mref. shorter
    	* also MonitorRef to Mref. same reason
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_tcprx.erl
+++ src/smpp34_tcprx.erl
@@ -13,18 +13,18 @@
         code_change/3]).
 
 
--record(state, {owner,monitref,socket,pdurx,data}).
+-record(state, {owner,mref,socket,pdusink,data}).
 
-start_link(Owner, Socket, PduRx) ->
-    gen_server:start_link(?MODULE, [Owner, Socket, PduRx], []).
+start_link(Owner, Socket, PduSink) ->
+    gen_server:start_link(?MODULE, [Owner, Socket, PduSink], []).
 
 stop(Pid) ->
     gen_server:cast(Pid, stop).
 
-init([Owner, Socket, PduRx]) ->
-	MonitorRef = erlang:monitor(process, Owner),
-    {ok, #state{owner=Owner, monitref=MonitorRef, socket=Socket,
-				   pdurx=PduRx, data = <<>>}}.
+init([Owner, Socket, PduSink]) ->
+	Mref = erlang:monitor(process, Owner),
+    {ok, #state{owner=Owner, mref=Mref, socket=Socket,
+				   pdusink=PduSink, data = <<>>}}.
 
 handle_call(Req, _From, St) ->
     {reply, {error, Req}, St}.
@@ -34,10 +34,10 @@ handle_cast(stop, St) ->
 handle_cast(_Req, St) ->
     {noreply, St}.
 
-handle_info({tcp, Socket, Data}, #state{socket=Socket, data=Data0, pdurx=PduRx}=St) ->
+handle_info({tcp, Socket, Data}, #state{socket=Socket, data=Data0, pdusink=PduSink}=St) ->
     Data1 = <<Data0/binary,Data/binary>>,
 	{_, PduList, Rest} = smpp34pdu:unpack(Data1), 
-	smpp34_pdurx:deliver(PduRx, PduList), 
+	smpp34_rx:deliver(PduSink, PduList), 
 	inet:setopts(Socket, [{active, once}]), 
 	{noreply, St#state{data=Rest}};
 handle_info({tcp_closed, Socket}, #state{socket=Socket}=St) ->
@@ -48,12 +48,12 @@ handle_info({tcp_error, Socket, Reason}, #state{socket=Socket}=St) ->
 	% the monitoring process will also bail.
 
 	{stop, {tcp_error, Reason}, St};
-handle_info(#'DOWN'{ref=MonitorRef}, #state{monitref=MonitorRef}=St) ->
+handle_info(#'DOWN'{ref=Mref}, #state{mref=Mref}=St) ->
 	{stop, normal, St};
 handle_info(_Req, St) ->
 	{noreply, St}.
 
-terminate(_Reason, _St) ->
+terminate(_, _) ->
     ok.
 
 code_change(_OldVsn, St, _Extra) ->
