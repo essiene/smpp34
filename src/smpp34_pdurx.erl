d67dadbeca233cafbf7568e854f98d40ec4d2548
Essien Ita Essien <essiene@gmail.com>
2010-08-26 13:15:24 +0100

    Implement smpp34_pdurx:deliver/2
    When called, this function delivers a proper asyncronous
    message to the smpp34_pdurx gen_server. Its cleaner than
    using the handle_info handler to process random messages
    being sent via Pid ! message to the process.
    
    In addition to the other pdus being handled here, smpp34_pdurx
    will now terminate with reason 'unbind_resp' if it receives
    an #unbind_resp{} pdu from the other side
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_pdurx.erl
+++ src/smpp34_pdurx.erl
@@ -4,7 +4,7 @@
 -behaviour(gen_server).
 
 -export([start_link/2,stop/1]).
--export([ping/1]).
+-export([ping/1, deliver/2]).
 
 -export([init/1,
         handle_call/3,
@@ -24,6 +24,14 @@ stop(Pid) ->
 ping(Pid) ->
 	gen_server:call(Pid, ping).
 
+deliver(_, []) ->
+	ok;
+deliver(Pid, [Head|Rest]) ->
+	deliver(Pid, Head),
+	deliver(Pid, Rest);
+deliver(Pid, Pdu) ->
+	gen_server:cast(Pid, Pdu).
+
 init([Owner, Tx]) ->
 	MonitorRef = erlang:monitor(process, Owner),
     {ok, #state{owner=Owner, monitref=MonitorRef, tx=Tx}}.
@@ -33,22 +41,23 @@ handle_call(ping, _From, #state{owner=Owner, tx=Tx}=St) ->
 handle_call(Req, _From, St) ->
     {reply, {error, Req}, St}.
 
+handle_cast(#pdu{sequence_number=Snum, body=#enquire_link{}}, #state{tx=Tx}=St) ->	
+	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #enquire_link_resp{}),
+	{noreply, St};
+handle_cast(#pdu{sequence_number=Snum, body=#unbind{}}, #state{tx=Tx}=St) ->	
+	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #unbind_resp{}),
+	{stop, unbind, St};
+handle_cast(#pdu{body=#unbind_resp{}}, #state{tx=Tx}=St) ->	
+	{stop, unbind_resp, St};
+handle_cast(#pdu{}=Pdu, #state{owner=Owner}=St) ->
+	error_logger:info_msg("Sending pdu to owner(~p): ~p~n", [Owner, Pdu]),
+	Owner ! {self(), Pdu},
+	{noreply, St};
 handle_cast(stop, St) ->
     {stop, normal, St};
 handle_cast(_Req, St) ->
     {noreply, St}.
 
-handle_info(#pdu{sequence_number=Snum, body=#enquire_link{}},
-		    #state{tx=Tx}=St) ->	
-	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #enquire_link_resp{}),
-	{noreply, St};
-handle_info(#pdu{sequence_number=Snum, body=#unbind{}},
-		    #state{tx=Tx}=St) ->	
-	smpp34_tcptx:send(Tx, ?ESME_ROK, Snum, #unbind_resp{}),
-	{stop, unbind, St};
-handle_info(#pdu{}=Pdu, #state{owner=Owner}=St) ->
-	Owner ! {self(), Pdu},
-	{noreply, St};
 handle_info(#'DOWN'{ref=MonitorRef}, #state{monitref=MonitorRef}=St) ->
 	{stop, normal, St};
 handle_info(_Req, St) ->
