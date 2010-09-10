42297f466ff4ac13eaa474f557211b7e087b4434
Essien Ita Essien <essiene@gmail.com>
2010-09-10 17:28:42 +0100

    Update smpp34_esme to use smpp34_esme_core instead.
    smpp34_esme is now a simple gen_server that calls
    smpp34_esme_core.
    
    Messages are delivered to it by esme_core.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_esme.erl
+++ src/smpp34_esme.erl
@@ -1,154 +1,78 @@
 -module(smpp34_esme).
--behaviour(gen_fsm).
-
--include_lib("smpp34pdu/include/smpp34pdu.hrl").
+-behaviour(gen_server).
 -include("util.hrl").
 
 -define(ETS_OPTS, [ordered_set, private, {keypos, 2}]).
--define(SOCK_OPTS, [binary, {packet, raw}, {active, once}]).
 
--record(st, {owner, mref,
-			 tx, tx_mref, 
-		     rx, rx_mref,
-			 ets, params,
-			 socket,
-			 close_reason}).
+-record(st, {esme, esme_mref, ets}).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/3, connect/2, close/1, send/2, send/3]).
+-export([connect/2, close/1, send/2, send/3]).
 
 %% ------------------------------------------------------------------
-%% gen_fsm Function Exports
+%% gen_server Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, open/2, closed/2, open/3, closed/3, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link(Owner, Host, Port) ->
-  gen_fsm:start(?MODULE, [Owner, Host, Port], []).
-
 connect(Host, Port) ->
-  smpp34_esme_sup:start_child(Host, Port).
+  gen_server:start(?MODULE, [Host, Port], []).
 
 close(Pid) ->
-	gen_fsm:sync_send_all_state_event(Pid, close).
+	gen_server:call(Pid, close).
 
 send(Pid, Body) ->
-	send(Pid, ?ESME_ROK, Body).
+	gen_server:call(Pid, {send, Body}).
 
 send(Pid, Status, Body) ->
-	gen_fsm:sync_send_event(Pid, {tx, Status, Body}).
+	gen_server:call(Pid, {send, Status, Body}).
 
 %% ------------------------------------------------------------------
-%% gen_fsm Function Definitions
+%% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init([Owner, Host, Port]) ->
-	process_flag(trap_exit, true),
-	Mref = erlang:monitor(process, Owner),
-	St = #st{owner=Owner, mref=Mref},
-	case gen_tcp:connect(Host, Port, ?SOCK_OPTS) of
+init([Host, Port]) ->
+	case smpp34_esme_core_sup:start_child(Host, Port) of
+		{ok, Esme} ->
+			Mref = erlang:monitor(process, Esme),
+			% Possible optimization: use same ETS among all instances
+			Ets = ets:new(esme_ets, ?ETS_OPTS),
+			St = #st{esme=Esme, esme_mref=Mref, ets=Ets},
+			{ok, St};
 		{error, Reason} ->
-			{stop, Reason};
-		{ok, Socket} ->
-			St0 = St#st{socket=Socket},
-
-			case smpp34_tx_sup:start_child(Socket) of
-				{error, Reason} ->
-					{stop, Reason};
-				{ok, Tx} ->
-					St1 = St0#st{tx=Tx},
-
-					TxMref = erlang:monitor(process, Tx),
-					St2 = St1#st{tx_mref=TxMref},
-
-					case smpp34_rx_sup:start_child(Tx, Socket) of
-						{error, Reason} ->
-							{stop, Reason};
-						{ok, Rx} ->
-							St3 = St2#st{rx=Rx},
-
-							RxMref = erlang:monitor(process, Rx),
-							St4 = St3#st{rx_mref=RxMref},
-
-							case smpp34_rx:controll_socket(Rx, Socket) of
-								{error, Reason} ->
-									{stop, Reason};
-								ok ->
-									Ets = ets:new(pdus, ?ETS_OPTS),
-									{ok, open, St4#st{ets=Ets, params={Host, Port}}}
-							end
-					end
-			end
+			{stop, Reason}
 	end.
 
-open(_Event, St) ->
-  {next_state, open, St}.
- 
-closed(_Event, St) ->
-  {next_state, closed, St}.
+handle_call(close, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:stop(E), St};
+handle_call({send, Body}, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:send(E, Body), St};
+handle_call({send, Status, Body}, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:send(E, Status, Body), St};
+handle_call(R, _From, St) ->
+  {reply, {error, R}, St}.
 
+handle_cast(_R, St) ->
+  {noreply, St}.
 
-open({tx, Status, Body}, _From, #st{tx=Tx}=St) ->
-  {reply, catch(smpp34_tx:send(Tx, Status, Body)), open, St};
-open(_Event, _From, St) ->
-  {reply, {error, _Event}, open, St}.
-
-closed(_Event, _From, #st{close_reason=undefined}=St) ->
-  {reply, {error, closed}, closed, St};
-closed(_Event, _From, #st{close_reason={error, R}}=St) ->
-  {reply, {error, R}, closed, St};
-closed(_Event, _From, #st{close_reason=R}=St) ->
-  {reply, {error, R}, closed, St}.
-
-handle_event(_Event, StateName, St) ->
-  {next_state, StateName, St}.
-
-handle_sync_event(close, _From, closed, St) ->
-	{reply, {error, closed}, closed, St};
-handle_sync_event(close, _From, _, St) ->
-	do_stop(close, St),
-	{reply, ok, closed, St};
-handle_sync_event(_Event, _From, StateName, St) ->
-  {reply, ok, StateName, St}.
-
-handle_info({Rx, Pdu}, StateName, #st{rx=Rx}=St) ->
-  error_logger:info_msg("ESME PDU> ~p~n", [Pdu]),
-  {next_state, StateName, St};
-handle_info(#'DOWN'{reason=normal}, _, St) ->
-  {next_state, closed, St};
-handle_info(#'DOWN'{ref=MRef, reason=R}, _, #st{mref=MRef}=St) ->
+handle_info({esme_data, E, Pdu}, #st{esme=E}=St) ->
+  error_logger:info_msg("PDU ==> ~p~n", [Pdu]),
+  {noreply, St};
+handle_info(#'DOWN'{ref=MRef, reason=R}, #st{esme_mref=MRef}=St) ->
   {stop, R, St};
-handle_info(#'DOWN'{ref=MRef, reason=R}, _, #st{tx_mref=MRef}=St) ->
-  do_stop(tx, St),
-  {next_state, closed, St#st{close_reason=R}};
-handle_info(#'DOWN'{ref=MRef, reason=R}, _, #st{rx_mref=MRef}=St) ->
-  do_stop(rx, St),
-  {next_state, closed, St#st{close_reason=R}};
-handle_info(_Info, StateName, St) ->
-  {next_state, StateName, St}.
+handle_info(_Info, St) ->
+  {noreply, St}.
 
-terminate(_, _, #st{ets=Ets}) ->
+terminate(_, #st{ets=Ets}) ->
  ets:delete(Ets),
  ok.
 
-code_change(_OldVsn, StateName, St, _Extra) ->
-  {ok, StateName, St}.
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-
-do_stop(close, #st{tx=Tx, rx=Rx}) ->
-	catch(smpp34_tx:stop(Tx)),
-	catch(smpp34_rx:stop(Rx));
-do_stop(rx, #st{tx=Tx}) ->
-	catch(smpp34_tx:stop(Tx));
-do_stop(tx, #st{rx=Rx}) ->
-	catch(smpp34_rx:stop(Rx)).
+code_change(_OldVsn, St, _Extra) ->
+  {ok, St}.
