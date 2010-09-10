73a3bef8187b67058445365aeeaaf29c82665a27
Essien Ita Essien <essiene@gmail.com>
2010-09-10 17:28:16 +0100

    Add supervisor for esme_core and update smpp34_sup
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_esme_core_sup.erl
+++ src/smpp34_esme_core_sup.erl
@@ -1,16 +1,16 @@
 
--module(smpp34_sup).
+-module(smpp34_esme_core_sup).
 
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_child/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, infinity, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -19,14 +19,13 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_child(Host, Port) ->
+	supervisor:start_child(?MODULE, [self(), Host, Port]).
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, [?CHILD(smpp34_snum_sup, supervisor),
-			 					  ?CHILD(smpp34_tcprx_sup, supervisor),
-								  ?CHILD(smpp34_tx_sup, supervisor),
-								  ?CHILD(smpp34_rx_sup, supervisor),
-								  ?CHILD(smpp34_esme_core_sup, supervisor)]}}.
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(smpp34_esme_core, worker)]} }.
 
