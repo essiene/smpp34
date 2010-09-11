ed3c3667977646a29e1027346ad1bab963f604b4
Essien Ita Essien <essiene@gmail.com>
2010-09-11 20:31:34 +0100

    smpp34_esme is now a gen_fsm
    - There are now two states, open and close. smpp34_esme never dies
      but just changes state to closed.
    - Track the close_reason and possibly report back to caller
    - Ensure that a RECV will ALWAYS get a response, and never times
      out indefinitely. This is done by keeping an esme_core error message
      in the pduq untill a recv call matches it.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_esme.erl
+++ src/smpp34_esme.erl
@@ -1,8 +1,8 @@
 -module(smpp34_esme).
--behaviour(gen_server).
+-behaviour(gen_fsm).
 -include("util.hrl").
 
--record(st, {esme, esme_mref, pduq, recvq}).
+-record(st, {esme, esme_mref, pduq, recvq, close_reason}).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -11,43 +11,72 @@
 -export([connect/2, close/1, send/2, send/3, recv/1, recv/2]).
 
 %% ------------------------------------------------------------------
-%% gen_server Function Exports
+%% gen_fsm Function Exports
 %% ------------------------------------------------------------------
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, open/2, closed/2, open/3, closed/3, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 connect(Host, Port) ->
-  gen_server:start(?MODULE, [Host, Port], []).
+  gen_fsm:start_link(?MODULE, [Host, Port], []).
 
 close(Pid) ->
-	gen_server:call(Pid, close).
+	gen_fsm:sync_send_event(Pid, close).
 
 send(Pid, Body) ->
-	gen_server:call(Pid, {send, Body}).
+	gen_fsm:sync_send_event(Pid, {send, Body}).
 
 send(Pid, Status, Body) ->
-	gen_server:call(Pid, {send, Status, Body}).
+	gen_fsm:sync_send_event(Pid, {send, Status, Body}).
 
 recv(Pid) ->
 	% by default block forever till response comes back
 	recv(Pid, infinity).
 
 recv(Pid, Timeout) ->
-	case catch(gen_server:call(Pid, {recv, Timeout}, Timeout)) of
+	case catch(gen_fsm:sync_send_event(Pid, {recv, Timeout}, Timeout)) of
 		{'EXIT', {R, _}} ->
 			{error, R};
 		Other ->
 			Other
 	end.
+
+%% ------------------------------------------------------------------
+%% gen_fsm States
+%% ------------------------------------------------------------------
+
 		
+
+open(_Event, St) ->
+  {next_state, open, St}.
+ 
+closed(_Event, St) ->
+  {next_state, closed, St}.
+
+open(close, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:stop(E), closed, St};
+open({send, Body}, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:send(E, Body), open, St};
+open({send, Status, Body}, _F, #st{esme=E}=St) ->
+  {reply, smpp34_esme_core:send(E, Status, Body), open, St};
+open({recv, Timeout}, From, St) ->
+	Item = dkq:item(Timeout, From),
+	do_recv(St, open, Item);
+open(_Event, _From, St) ->
+  {reply, {error, _Event}, open, St}.
+
+closed(_Event, _From, #st{close_reason=undefined}=St) ->
+  {reply, {error, closed}, closed, St};
+closed(_Event, _From, #st{close_reason={error, R}}=St) ->
+  {reply, {error, R}, closed, St};
+closed(_Event, _From, #st{close_reason=R}=St) ->
+  {reply, {error, R}, closed, St}.
 
 
 %% ------------------------------------------------------------------
-%% gen_server Function Definitions
+%% gen_fsm Function Definitions
 %% ------------------------------------------------------------------
 
 init([Host, Port]) ->
@@ -57,58 +86,62 @@ init([Host, Port]) ->
 			PduQ = queue:new(),
 			RecvQ = dkq:new(),
 			St = #st{esme=Esme, esme_mref=Mref, pduq=PduQ, recvq=RecvQ},
-			{ok, St};
+			{ok, open, St};
 		{error, Reason} ->
 			{stop, Reason}
 	end.
 
-handle_call(close, _F, #st{esme=E}=St) ->
-  {reply, smpp34_esme_core:stop(E), St};
-handle_call({send, Body}, _F, #st{esme=E}=St) ->
-  {reply, smpp34_esme_core:send(E, Body), St};
-handle_call({send, Status, Body}, _F, #st{esme=E}=St) ->
-  {reply, smpp34_esme_core:send(E, Status, Body), St};
-handle_call({recv, Timeout}, From, St) ->
-	Item = dkq:item(Timeout, From),
-	do_recv(St, Item);
-handle_call(R, _From, St) ->
-  {reply, {error, R}, St}.
 
-handle_cast(_R, St) ->
-  {noreply, St}.
+handle_event(_Event, StateName, St) ->
+  {next_state, StateName, St}.
 
-handle_info({esme_data, E, Pdu}, #st{esme=E}=St) ->
-  do_pdu_recv(St, Pdu);
-handle_info(#'DOWN'{ref=MRef, reason=R}, #st{esme_mref=MRef}=St) ->
-  {stop, R, St};
-handle_info(_Info, St) ->
-  {noreply, St}.
 
-terminate(_, _) ->
+handle_sync_event(_Event, _From, StateName, St) ->
+  {reply, ok, StateName, St}.
+
+
+handle_info({esme_data, E, Pdu}, StateName, #st{esme=E}=St) ->
+  do_pdu_recv(St, StateName, Pdu);
+handle_info(#'DOWN'{ref=MRef, reason=R}, StateName, #st{esme_mref=MRef}=St) ->
+  % when esme_core dies, keep a response for any caller that may be 
+  % recv'ing, especially important if timeout is infinity. So they don't
+  % wait forever. This way, EVERY RECV WILL ALWAYS GET A RESPONSE
+  do_pdu_recv(St, StateName, R),
+  {next_state, closed, St#st{close_reason=R}};
+handle_info(_Info, StateName, St) ->
+  {next_state, StateName, St}.
+
+terminate(_, _, _) ->
  ok.
 
-code_change(_OldVsn, St, _Extra) ->
-  {ok, St}.
+code_change(_OldVsn, StateName, St, _Extra) ->
+  {ok, StateName, St}.
 
-do_recv(#st{pduq=PduQ, recvq=RecvQ}=St, Item) ->
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+
+do_recv(#st{pduq=PduQ, recvq=RecvQ}=St, StateName, Item) ->
 	case queue:out(PduQ) of
 		{{value, Data}, PduQ1} ->
-			{reply, {ok, Data}, St#st{pduq=PduQ1}};
+			{reply, {ok, Data}, StateName, St#st{pduq=PduQ1}};
 		{empty, PduQ} ->
 			RecvQ1 = dkq:in(Item, RecvQ),
-			{noreply, St#st{recvq=RecvQ1}}
+			{next_state, StateName, St#st{recvq=RecvQ1}}
 	end.
 
-do_pdu_recv(#st{pduq=PduQ, recvq=RecvQ}=St, Pdu) ->
+do_pdu_recv(#st{pduq=PduQ, recvq=RecvQ}=St, StateName, Pdu) ->
 	case dkq:out(RecvQ) of
 		{empty, RecvQ} ->
 			PduQ1 = queue:in(Pdu, PduQ),
-			{noreply, St#st{pduq=PduQ1}};
+			{next_state, StateName, St#st{pduq=PduQ1}};
 		{{value, From}, RecvQ1} ->
-			gen_server:reply(From, {ok, Pdu}),
-			{noreply, St#st{recvq=RecvQ1}};
+			gen_fsm:reply(From, {ok, Pdu}),
+			{next_state, StateName, St#st{recvq=RecvQ1}};
 		{{decayed, _}, RecvQ1} ->
 			St1 = St#st{recvq=RecvQ1},
-			do_pdu_recv(St1, Pdu)
+			do_pdu_recv(St1, StateName, Pdu)
 	end.
 			
