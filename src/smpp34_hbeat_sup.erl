6aade60a876da3eb35bd5ccb3ddfb5cc2065d127
Essien Ita Essien <essiene@gmail.com>
2010-11-04 23:30:53 +0100

    Implement the hearbeat supervisor module
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/smpp34_hbeat_sup.erl
@@ -0,0 +1,31 @@
+
+-module(smpp34_hbeat_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start_child/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_child(Tx) ->
+	supervisor:start_child(?MODULE, [self(), Tx]).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(smpp34_hbeat, worker)]} }.
+
