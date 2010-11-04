c1dc492fe4b57b0e3d2f240b23976bfc43d25eb1
Essien Ita Essien <essiene@gmail.com>
2010-11-05 00:14:55 +0100

    Integrate smpp34_hbeat into smpp34_rx module
    - Add the hb fields to rx state record
    - Start hbeat in rx _after_ tcprx has been started. monitor hbeat
    - When enquire_link_resp is received call hbeat's enquire_link_resp
      notifier function
    - shutdown when hbeat dies
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_rx.erl
+++ src/smpp34_rx.erl
@@ -13,7 +13,7 @@
         terminate/2,
         code_change/3]).
 
--record(state, {owner, mref, tx, tx_mref, rx, rx_mref}).
+-record(state, {owner, mref, tx, tx_mref, rx, rx_mref, hb, hb_mref}).
 
 start_link(Owner, Tx, Socket) ->
     gen_server:start_link(?MODULE, [Owner, Tx, Socket], []).
@@ -45,10 +45,17 @@ init([Owner, Tx, Socket]) ->
 			{stop, Reason};
 		{ok, Rx} ->
 			RxMref = erlang:monitor(process, Rx),
-			{ok, 
-				#state{owner=Owner, mref=MRef, 
-					   tx=Tx, tx_mref=TxMref,
-					   rx=Rx, rx_mref=RxMref}}
+            case smpp34_hbeat_sup:start_child(Tx) of
+                {error, Reason} ->
+                    {stop, Reason};
+                {ok, Hb} ->
+                    HbMref = erlang:monitor(process, Hb),
+                    {ok, 
+                        #state{owner=Owner, mref=MRef, 
+                               tx=Tx, tx_mref=TxMref,
+                               rx=Rx, rx_mref=RxMref,
+                               hb=Hb, hb_mref=HbMref}}
+             end
 	end.
 
 handle_call(getrx, _From, #state{rx=Rx}=St) ->
@@ -66,6 +73,9 @@ handle_cast({Rx, #pdu{sequence_number=Snum, body=#unbind{}}},
 					#state{tx=Tx, rx=Rx}=St) ->	
 	tx_send(Tx, ?ESME_ROK, Snum, #unbind_resp{}),
 	{stop, unbind, St};
+handle_cast({Rx, #pdu{body=#enquire_link_resp{}}}, #state{rx=Rx, hb=Hb}=St) ->	
+    smpp34_hbeat:enquire_link_resp(Hb),
+    {noreply, St};
 handle_cast({Rx, #pdu{body=#unbind_resp{}}}, #state{rx=Rx}=St) ->	
 	{stop, unbind_resp, St};
 handle_cast({Rx, #pdu{}=Pdu}, #state{owner=Owner, rx=Rx}=St) ->
@@ -79,6 +89,8 @@ handle_cast(_Req, St) ->
 handle_info(#'DOWN'{ref=MRef}, #state{tx_mref=MRef}=St) ->
 	{noreply, St#state{tx=undefined, tx_mref=undefined}};
 handle_info(#'DOWN'{ref=MRef, reason=R}, #state{rx_mref=MRef}=St) ->
+	{stop, R, St};
+handle_info(#'DOWN'{ref=MRef, reason=R}, #state{hb_mref=MRef}=St) ->
 	{stop, R, St};
 handle_info(#'DOWN'{ref=MRef}, #state{mref=MRef}=St) ->
 	{stop, normal, St};
