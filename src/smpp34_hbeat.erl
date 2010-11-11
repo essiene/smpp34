35d08f903f3d2bcdaca2c05010b59086f924a906
Essien Ita Essien <essiene@gmail.com>
2010-11-11 10:04:02 +0100

    Start converting smpp34_hbeat to a gen_fsm
    - First clearout all the gen_server callbacks replacing
      them with base gen_fsm callbacks
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_hbeat.erl
+++ src/smpp34_hbeat.erl
@@ -1,16 +1,16 @@
 -module(smpp34_hbeat).
 -include_lib("smpp34pdu/include/smpp34pdu.hrl").
 -include("util.hrl").
--behaviour(gen_server).
+-behaviour(gen_fsm).
 
 -export([start_link/2,stop/1, enquire_link_resp/1]).
 
 -export([init/1,
-        handle_call/3,
-        handle_cast/2,
-        handle_info/2,
-        terminate/2,
-        code_change/3]).
+        handle_sync_event/4,
+        handle_event/3,
+        handle_info/3,
+        terminate/3,
+        code_change/4]).
 
 -record(st_hbeat, {owner, tx, tx_tref, rx_tref, monitref}).
 
@@ -32,50 +32,19 @@ init([Owner, Tx]) ->
 	MonitorRef = erlang:monitor(process, Owner),
     {ok, #st_hbeat{owner=Owner, tx=Tx, monitref=MonitorRef}, 500}.
 
-handle_call({enquire_link_resp, Owner}, _F, #st_hbeat{owner=Owner}=St0) ->
-    St1 = sched_enquire_link(St0),
-    {reply, ok, St1};
-handle_call(Req, _From, St) ->
-    {reply, {error, Req}, St}.
 
-handle_cast(stop, St) ->
-    {stop, normal, St};
-handle_cast(_Req, St) ->
-    {noreply, St}.
 
-handle_info(timeout, St0) ->
-    St1 = send_enquire_link(St0),
-    {noreply, St1};
-handle_info({timeout, TxRef, heartbeat}, #st_hbeat{tx_tref=TxRef}=St0) ->
-    St1 = send_enquire_link(St0),
-    {noreply, St1};
-handle_info({timeout, RxRef, no_response}, #st_hbeat{rx_tref=RxRef}=St) ->
-    %log a warning and continue
-    {noreply, sched_enquire_link(St)};
-handle_info(#'DOWN'{ref=MonitorRef}, #st_hbeat{monitref=MonitorRef}=St) ->
-	{stop, normal, St};
-handle_info(_Req, St) ->
-    {noreply, St}.
+handle_sync_event(E, _F, State, StData) ->
+    {reply, {error, E}, State, StData}.
 
-terminate(_, _) ->
+handle_event(_E, State, StData) ->
+    {next_state, State, StData}.
+
+handle_info(_E, State, StData) ->
+    {next_state, State, StData}.
+
+terminate(_,_,_) ->
     ok.
 
-code_change(_OldVsn, St, _Extra) ->
-    {noreply, St}.
-
-send_enquire_link(#st_hbeat{tx=Tx}=St0) ->
-    {ok, _Snum} = smpp34_tx:send(Tx, ?ESME_ROK, #enquire_link{}),
-    RxRef = erlang:start_timer(?RESP_INTERVAL, self(), no_response),
-    St0#st_hbeat{rx_tref=RxRef, tx_tref=undefined}.
-
-sched_enquire_link(#st_hbeat{rx_tref=RxRef}=St0) ->
-    case RxRef of
-        undefined ->
-            ok;
-        R -> 
-            erlang:cancel_timer(R)
-    end,
-    TxRef = erlang:start_timer(?ENQ_LNK_INTERVAL, self(), heartbeat),
-    St0#st_hbeat{tx_tref=TxRef, rx_tref=undefined}.
-
-
+code_change(_OldVsn, State, StData, _Extra) ->
+    {ok, State, StData}.
