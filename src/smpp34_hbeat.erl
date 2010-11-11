cdd931a69bf8073ab1dcb79bde7eafa6380463a7
Essien Ita Essien <essiene@gmail.com>
2010-11-11 11:39:52 +0100

    Handle enquire_link_resp in transmit_scheduled state
    Handle the enquire_link_resp message which occurs while
    in the transmit_scheduled state.
    
    Properly update the reqs list, calculate the response
    time and update the resp_time field if greater than
    the previous one.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_hbeat.erl
+++ src/smpp34_hbeat.erl
@@ -44,6 +44,26 @@ transmit_scheduled(send_enquire_link, #st_hbeat{tx=Tx, reqs=Reqs0,
 
     {next_state, enquire_link_sent, St1#st_hbeat{reqs=Reqs1, rx_tref=Tref}};
 
+transmit_scheduled({enquire_link_resp, Snum, Owner}, #st_hbeat{owner=Owner,
+                    reqs=Reqs0, resp_time=RespTime1}=St0) ->
+    T2 = erlang:now(),
+    case lists:keytake(Snum, 1, Reqs0) of
+        false ->
+            %log response for unknown or garbage collected? request
+            {next_state, transmit_scheduled, St0};
+        {value, {Snum, T1}, Reqs1} ->
+            St1 = St0#st_hbeat{reqs=Reqs1},
+
+            case timer:now_diff(T1, T2) div 1000 of %convert to milliseconds from microseconds
+                N when N < RespTime1 ->
+                    %log response time for Snum
+                    {next_state, transmit_scheduled, St1};
+                N ->
+                    %log response time for Snum and changing resp_time to N
+                    {next_state, transmit_scheduled, St1#st_hbeat{resp_time=N}}
+            end
+    end;
+
 transmit_scheduled(_E, St) ->
     {next_state, transmit_scheduled, St}.
 
