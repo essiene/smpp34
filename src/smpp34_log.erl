e28a11a530ffd5c92a05c98fbe0c1b31c37ec206
Essien Ita Essien <essiene@gmail.com>
2010-12-11 22:44:37 +0100

    Flesh out smpp34_log  as an event manager
     * implement start, stop and add_logger
     * Modify log/3 to call gen_event:notify/2
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/smpp34_log.erl
+++ src/smpp34_log.erl
@@ -1,23 +1,37 @@
 -module(smpp34_log).
 
+-export([start_link/0, stop/1, add_logger/3]).
 -export([debug/2,info/2,error/2,warn/2]).
 
 
 
-debug(Pid, Term) ->
-    log(Pid, esme_log_debug, Term).
+start_link() ->
+    gen_event:start_link().
 
-info(Pid, Term) ->
-    log(Pid, esme_log_info, Term).
+stop(Ref) ->
+    gen_event:stop(Ref).
 
-error(Pid, Term) ->
-    log(Pid, esme_log_error, Term).
+add_logger(Ref, Logger, Args) ->
+    case gen_event:add_sup_handler(Ref, Logger, Args) of
+        ok ->
+            ok;
+        {'EXIT', Reason} ->
+            {error, Reason};
+        Other ->
+            {error, Other}
+    end.
 
-warn(Pid, Term) ->
-    log(Pid, esme_log_warn, Term).
+debug(Ref, Term) ->
+    log(Ref, esme_log_debug, Term).
 
-log(Pid, Tag, Term) when is_pid(Pid) ->
-    Pid ! {Tag, self(), Term};
-log(_,_,_) ->
-    ok;
+info(Ref, Term) ->
+    log(Ref, esme_log_info, Term).
 
+error(Ref, Term) ->
+    log(Ref, esme_log_error, Term).
+
+warn(Ref, Term) ->
+    log(Ref, esme_log_warn, Term).
+
+log(Ref, Tag, Term) ->
+    gen_event:notify(Ref, {Tag, Term}).
