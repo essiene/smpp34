a29565da2cebe3418cc8832ce66e055701a7831c
Essien Ita Essien <essiene@gmail.com>
2010-12-11 22:13:19 +0100

    Implement log supervisor
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/sup/smpp34_log_sup.erl
@@ -0,0 +1,31 @@
+
+-module(smpp34_log_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start_child/1]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_child() ->
+	supervisor:start_child(?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(smpp34_log, worker)]} }.
+
