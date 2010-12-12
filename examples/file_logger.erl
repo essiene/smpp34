ac2e9ed1c77de7dbcfd0271de69fbc58bceb1a0f
Essien Ita Essien <essiene@gmail.com>
2010-12-12 10:13:54 +0100

    Add a file logging log handler example
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ examples/file_logger.erl
@@ -0,0 +1,33 @@
+-module(file_logger).
+-behaviour(gen_event).
+
+-export([init/1,handle_event/2,handle_call/2,
+         handle_info/2,terminate/2,code_change/3]).
+
+-record(st_flogger, {filename, io_device}).
+
+
+init([FileName]) ->
+    {ok, IoDevice} = file:open(FileName, [append]),
+    {ok, #st_flogger{filename=FileName, io_device=IoDevice}}.
+
+handle_event({Level, Log}, #st_flogger{io_device=IoDev}=St) ->
+    io:format(IoDev, "~p: ~s~n", [Level, Log]),
+    {ok, St};
+handle_event(_, St) ->
+    {ok, St}.
+
+handle_call(_, St) ->
+    {ok, ok, St}.
+
+handle_info(_, St) ->
+    {ok, St}.
+
+terminate(_, #st_flogger{io_device=IoDev}) ->
+    io:format(IoDev, "Logger terminating~n"),
+    file:close(IoDev),
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {ok, St}.
+
