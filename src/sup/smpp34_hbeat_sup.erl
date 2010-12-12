592ab564a66a0ee8188e0695e8cbc071fac0642d
Essien Ita Essien <essiene@gmail.com>
2010-12-12 23:40:12 +0100

    Also update hbeat, snum and tcprx supervisors
    All supervisors are now aware of the Logger parameter
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/sup/smpp34_hbeat_sup.erl
+++ src/sup/smpp34_hbeat_sup.erl
@@ -4,7 +4,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0, start_child/1]).
+-export([start_link/0, start_child/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -19,8 +19,8 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-start_child(Tx) ->
-	supervisor:start_child(?MODULE, [self(), Tx]).
+start_child(Tx, Logger) ->
+	supervisor:start_child(?MODULE, [self(), Tx, Logger]).
 
 %% ===================================================================
 %% Supervisor callbacks
